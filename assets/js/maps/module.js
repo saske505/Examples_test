d5f3507b145ee055cf391dc0e4926e98c500fbd7
Stephan Maree <Stephan.Maree@kingprice.co.za>
2017-10-04 21:31:03 +0200

    added main map module

---
+++ assets/js/maps/module.js
@@ -0,0 +1,19 @@
+﻿//Main application module for  accessing map functions
+
+//example 
+
+var App = (function () {
+
+    var _name = "Map functions";
+
+
+    var result = function (val) {
+        return val;
+    }
+
+    return {
+        result : result
+    }
+
+})();
+
